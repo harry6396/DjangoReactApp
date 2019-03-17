37aedc5cb2a4b7169f763356609566ba90c0b91b
harry6396 <harrytyagi2371@gmail.com>
2019-03-17 11:16:15 +0530

    Added loader and changed the header text

--- frontendsln/src/App.js
+++ frontendsln/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import MenuBar from './menubar';
+import Loader from 'react-loader-spinner';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      toShowLoader:true
+    };
+  }
   render() {
     return (
       <div>
         <div className="headerContainer">
-          <div className="headerContainerText">Welcome to Django & React World!</div>
+          <div className="headerContainerText">Welcome to Flask & React World!</div>
           <input type="text" className="searchBar" placeholder="Type here"/>
           <div className="fa fa-search search-icon"></div>
           <MenuBar/>
+        </div>
+        <div className="dataContainer"> 
+        {this.state.toShowLoader?
+        <div className="loaderContainer">
+        <Loader 
+         type="Ball-Triangle"
+         color="#00BFFF"
+         height="100"	
+         width="100"/>
+        </div>
+        :""}
         </div>
       </div>
     );
